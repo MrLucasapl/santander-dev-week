47f7354e378f39a83575c3e7d481d127746bb5c8
MrLucasapl <lucas.apereira20@gmail.com>
2024-09-05 14:00:47 -0300

    expondo endpoints

---
+++ src/main/java/com/lucaspereira/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.lucaspereira.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucaspereira.domain.model.User;
+import com.lucaspereira.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        return ResponseEntity.ok(userCreated);
+    }
+
+}
